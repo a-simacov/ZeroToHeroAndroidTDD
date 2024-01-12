2abda9a38cbdd1e781763a52d5f5e8567d3d19c1
slax <slax.xals@gmail.com>
2024-01-13 01:01:30 +0200

    task/003 solved

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task003Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task003Test.kt
@@ -1,15 +1,27 @@
 package ru.easycode.zerotoheroandroidtdd
 
 import android.widget.LinearLayout
+import androidx.compose.ui.test.assertAll
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.filter
+import androidx.compose.ui.test.hasAnyChild
+import androidx.compose.ui.test.hasParent
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onChildren
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withParent
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import org.hamcrest.Matchers.allOf
 import org.junit.Assert.*
 import org.junit.Rule
@@ -26,10 +38,68 @@ class Task003Test {
     fun test_change_parent() {
         onView(
             allOf(
-                withId(R.id.titleTextView),
+                //withId(R.id.titleTextView),
                 withText("I am an Android Developer!"),
                 withParent(isAssignableFrom(LinearLayout::class.java))
             )
         ).check(matches(isDisplayed()))
+    }
+}
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class Task003TestCompose {
+
+    @get:Rule
+    val activityRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun test_change_parent() {
+        activityRule.onRoot(useUnmergedTree = true).printToLog("TERT")
+
+        val parent = activityRule.onNodeWithTag("column")
+        parent.assertExists()
+        val child = activityRule.onNodeWithTag("mainTextView")
+        child.assertIsDisplayed()
+
+        // 1 способ
+        activityRule
+            .onNode(
+                matcher = hasTestTag("mainTextView")
+                        and
+                        hasText("I am an Android Developer!")
+                        and
+                        hasParent(
+                            hasTestTag("column")
+                        ),
+                useUnmergedTree = true
+            ).assertIsDisplayed()
+
+        // 2 способ
+        activityRule
+            .onNode(
+                matcher = hasTestTag("column")
+                        and
+                        hasAnyChild(
+                            hasTestTag("mainTextView")
+                                    and
+                                    hasText("I am an Android Developer!")
+                        ),
+                useUnmergedTree = true
+            )
+            .assertExists()
+            .assertIsDisplayed()
+
+        // 3 способ
+        activityRule
+            .onNode(matcher = hasTestTag("column"))
+            .assertExists()
+            .onChildren()
+            .filter(
+                hasTestTag("mainTextView")
+            )
+            .assertAll(
+                hasText("I am an Android Developer!")
+            )
     }
 }
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,31 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Column(
+                modifier = Modifier.fillMaxSize().testTag("column"),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "I am an Android Developer!",
+                    modifier = Modifier.testTag("mainTextView")
+                )
+            }
+        }
     }
 }
