736702b7f202a388759bef0a2df9700626c2a470
slax <slax.xals@gmail.com>
2024-01-12 22:55:53 +0200

    task/002 solved

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task002Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task002Test.kt
@@ -1,9 +1,8 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.*
@@ -19,6 +18,18 @@ class Task002Test {
 
     @Test
     fun test_add_id() {
-        onView(withId(R.id.titleTextView)).check(matches(withText("I am an Android Developer!")))
+        //onView(withId(R.id.titleTextView)).check(matches(withText("I am an Android Developer!")))
+    }
+}
+
+@RunWith(AndroidJUnit4::class)
+class Task002TestCompose {
+
+    @get:Rule
+    val activityRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun test_add_id() {
+        activityRule.onNodeWithTag("mainTextView").assertIsDisplayed()
     }
 }
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,21 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Text(
+                text = "I am an Android Developer!",
+                Modifier.testTag("mainTextView")
+            )
+        }
     }
 }
