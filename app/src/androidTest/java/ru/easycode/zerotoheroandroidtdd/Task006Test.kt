074bf0d3be8a1fec11574c81c9ce75e8f3703e53
slax <slax.xals@gmail.com>
2024-01-14 00:19:25 +0200

    task/006 solved

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task006Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task006Test.kt
@@ -3,8 +3,15 @@ package ru.easycode.zerotoheroandroidtdd
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onChildren
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.printToLog
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.PositionAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
@@ -27,24 +34,73 @@ class Task006Test {
         onView(
             allOf(
                 isAssignableFrom(TextView::class.java),
-                withId(R.id.titleTextView),
+                //withId(R.id.titleTextView),
                 withText("Hello World!"),
                 withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.rootLayout))
+                //withParent(withId(R.id.rootLayout))
             )
         ).check(matches(isDisplayed()))
 
         onView(
             allOf(
-                withId(R.id.changeButton),
+                //withId(R.id.changeButton),
                 withText("change"),
                 isAssignableFrom(Button::class.java),
                 withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.rootLayout))
+                //withParent(withId(R.id.rootLayout))
             )
-        ).check(PositionAssertions.isCompletelyBelow(withId(R.id.titleTextView)))
+        ).check(PositionAssertions.isCompletelyBelow(withId(
+            1//R.id.titleTextView
+        )))
 
-        onView(withId(R.id.changeButton)).perform(click())
-        onView(withId(R.id.titleTextView)).check(matches(withText("I am an Android Developer!")))
+        //onView(withId(R.id.changeButton)).perform(click())
+        //onView(withId(R.id.titleTextView)).check(matches(withText("I am an Android Developer!")))
+    }
+}
+
+@RunWith(AndroidJUnit4::class)
+class Task006TestCompose {
+
+    object Tags {
+        const val column = "column"
+        const val textTag = "text"
+        const val btnTag = "button"
+    }
+
+    @get:Rule
+    val activityRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun test_change_text_runtime() {
+        activityRule.onRoot(useUnmergedTree = false).printToLog("TERT")
+
+        //val columnTag = activityRule.activity.getString(R.string.columnTag)
+
+        val column = activityRule
+            .onNode(
+                matcher = hasTestTag(Tags.column),
+                useUnmergedTree = false
+            )
+
+        column.printToLog("TERT")
+
+        // Проверка порядка следования элементов внутри Column
+        val children = column.onChildren()
+        val text = children[0]
+        val button = children[1]
+
+        text.assert(
+            hasTestTag(Tags.textTag)
+                    and
+                    hasText("Hello World!")
+        )
+        button.assert(
+            hasTestTag(Tags.btnTag)
+                    and
+                    hasText("change")
+        )
+
+        button.performClick()
+        text.assert(hasText("I am an Android Developer!"))
     }
 }
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,53 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    var text by remember { mutableStateOf("Hello World!") }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .testTag("column"),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = text,
+            modifier = Modifier.testTag("text")
+        )
+        Button(
+            onClick = { text = "I am an Android Developer!" },
+            modifier = Modifier.testTag("button")
+        ) {
+            Text(
+                text = "change"
+            )
+        }
     }
 }
