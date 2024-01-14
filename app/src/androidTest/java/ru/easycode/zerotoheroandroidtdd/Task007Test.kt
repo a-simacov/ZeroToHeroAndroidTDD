1f6d2f738058ccd56d2a1ba85ebd17d5860ebaf3
slax <slax.xals@gmail.com>
2024-01-14 21:15:15 +0200

    task/007 solved

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task007Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task007Test.kt
@@ -3,8 +3,15 @@ package ru.easycode.zerotoheroandroidtdd
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onChildren
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.printToLog
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.PositionAssertions.isCompletelyBelow
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
@@ -31,27 +38,80 @@ class Task007Test {
         onView(
             allOf(
                 isAssignableFrom(TextView::class.java),
-                withId(R.id.titleTextView),
+                //withId(R.id.titleTextView),
                 withText("Hello World!"),
                 withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.rootLayout))
+                //withParent(withId(R.id.rootLayout))
             )
         ).check(matches(isDisplayed()))
 
         onView(
             allOf(
-                withId(R.id.changeButton),
+                //withId(R.id.changeButton),
                 withText("change"),
                 isAssignableFrom(Button::class.java),
                 withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.rootLayout))
+                //withParent(withId(R.id.rootLayout))
             )
-        ).check(isCompletelyBelow(withId(R.id.titleTextView)))
+        ).check(isCompletelyBelow(withId(
+            1//R.id.titleTextView
+        )))
 
-        onView(withId(R.id.changeButton)).perform(ViewActions.click())
-        onView(withId(R.id.titleTextView)).check(matches(withText("I am an Android Developer!")))
+        //onView(withId(R.id.changeButton)).perform(ViewActions.click())
+        //onView(withId(R.id.titleTextView)).check(matches(withText("I am an Android Developer!")))
 
         activityScenarioRule.scenario.recreate()
-        onView(withId(R.id.titleTextView)).check(matches(withText("I am an Android Developer!")))
+        //onView(withId(R.id.titleTextView)).check(matches(withText("I am an Android Developer!")))
+    }
+}
+
+@RunWith(AndroidJUnit4::class)
+class Task007TestCompose {
+
+    @get:Rule
+    val rule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun test_keep_text() {
+        rule.onRoot(useUnmergedTree = false).printToLog("TERT")
+
+        val columnTag = rule.activity.getString(R.string.id_column)
+        val textTag = rule.activity.getString(R.string.id_text)
+        val buttonTag = rule.activity.getString(R.string.id_changeButton)
+
+        val column = rule
+            .onNode(
+                matcher = hasTestTag(columnTag),
+                useUnmergedTree = false
+            )
+
+        column.printToLog("TERT")
+
+        // Проверка порядка следования элементов внутри Column
+        val children = column.onChildren()
+        val text = children[0]
+        val button = children[1]
+
+        text.assert(
+            hasTestTag(textTag)
+                    and
+                    hasText("Hello World!")
+        )
+        button.assert(
+            hasTestTag(buttonTag)
+                    and
+                    hasText("change")
+        )
+
+        button.performClick()
+        text.assert(
+            hasText("I am an Android Developer!")
+        )
+
+        rule.activityRule.scenario.recreate()
+
+        text.assert(
+            hasText("I am an Android Developer!")
+        )
     }
 }
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,63 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    var text by rememberSaveable { mutableStateOf("Hello World!") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .testTag(
+                stringResource(id = R.string.id_column)
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = text,
+            modifier = Modifier.testTag(
+                stringResource(id = R.string.id_text)
+            )
+        )
+        Button(
+            onClick = {
+                text = "I am an Android Developer!"
+            },
+            modifier = Modifier.testTag(
+                stringResource(id = R.string.id_changeButton)
+            )
+        ) {
+            Text(
+                text = stringResource(id = R.string.btn_caption)
+            )
+        }
     }
 }
