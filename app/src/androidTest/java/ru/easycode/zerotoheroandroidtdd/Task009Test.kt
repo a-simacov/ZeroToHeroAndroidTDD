639f31f82e1f948b1671ae51e90827a23fa91e61
slax <slax.xals@gmail.com>
2024-01-15 20:36:44 +0200

    task/009 solved by oop

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task009Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task009Test.kt
@@ -3,10 +3,19 @@ package ru.easycode.zerotoheroandroidtdd
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onChildren
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.printToLog
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.PositionAssertions.isCompletelyBelow
-import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
@@ -32,27 +41,96 @@ class Task009Test {
         onView(
             allOf(
                 isAssignableFrom(TextView::class.java),
-                withId(R.id.titleTextView),
+                //withId(R.id.titleTextView),
                 withText("Hello World!"),
                 withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.rootLayout))
+                //withParent(withId(R.id.rootLayout))
             )
         ).check(matches(isDisplayed()))
 
         onView(
             allOf(
-                withId(R.id.removeButton),
+                //withId(R.id.removeButton),
                 withText("remove"),
                 isAssignableFrom(Button::class.java),
                 withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.rootLayout))
+                //withParent(withId(R.id.rootLayout))
             )
-        ).check(isCompletelyBelow(withId(R.id.titleTextView)))
+        ).check(
+            isCompletelyBelow(
+                withId(
+                    1//R.id.titleTextView
+                )
+            )
+        )
 
-        onView(withId(R.id.removeButton)).perform(click())
-        onView(withId(R.id.titleTextView)).check(doesNotExist())
+//        onView(withId(R.id.removeButton)).perform(click())
+//        onView(withId(R.id.titleTextView)).check(doesNotExist())
 
         activityScenarioRule.scenario.recreate()
-        onView(withId(R.id.titleTextView)).check(doesNotExist())
+        //onView(withId(R.id.titleTextView)).check(doesNotExist())
+    }
+}
+
+@RunWith(AndroidJUnit4::class)
+class Task009TestCompose {
+
+    @get:Rule
+    val rule = createAndroidComposeRule<MainActivity>()
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun test_remove_textview() {
+        rule.onRoot(useUnmergedTree = false).printToLog("TERT")
+
+        val columnTag = rule.activity.getString(R.string.id_column)
+        val textTag = rule.activity.getString(R.string.id_text)
+        val btnTag = rule.activity.getString(R.string.id_remove_button)
+
+        val textValue = rule.activity.getString(R.string.hello_world)
+        val btnCaption = rule.activity.getString(R.string.btn_remove_caption)
+
+        val column = rule
+            .onNode(
+                matcher = hasTestTag(columnTag),
+                useUnmergedTree = false
+            )
+
+        // Проверка порядка следования элементов внутри Column
+        val children = column.onChildren()
+        val text = children[0]
+        val button = children[1]
+
+        text.assert(
+            hasTestTag(textTag)
+                    and
+                    hasText(textValue)
+        ).assertIsDisplayed()
+
+        button.assert(
+            hasTestTag(btnTag)
+                    and
+                    hasText(btnCaption)
+        ).assertIsDisplayed()
+
+        button.performClick()
+
+        // 1 способ
+        rule.waitUntilDoesNotExist(
+            hasTestTag(textTag)
+        )
+
+        // 2 способ
+        rule.waitUntil {
+            rule.onAllNodesWithTag(textTag)
+                .fetchSemanticsNodes()
+                .isEmpty()
+        }
+
+        rule.activityRule.scenario.recreate()
+
+        rule.waitUntilDoesNotExist(
+            hasTestTag(textTag)
+        )
     }
 }
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,83 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MainScreen()
+        }
     }
+}
+
+@Composable
+fun MainScreen() {
+    var textState: TextState by rememberSaveable { mutableStateOf(TextState.Initial) }
+    val btnOnClick = { textState = TextState.Removed }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .addTag(id = R.string.id_column),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        textState.Apply()
+        Button(
+            onClick = btnOnClick,
+            modifier = Modifier.addTag(id = R.string.id_remove_button)
+        ) {
+            Text(
+                text = stringResource(id = R.string.btn_remove_caption)
+            )
+        }
+    }
+}
+
+interface TextState : Serializable {
+
+    @Composable
+    fun Apply()
+
+    object Initial: TextState {
+
+        @Composable
+        override fun Apply() {
+            Text(
+                text = stringResource(id = R.string.hello_world),
+                modifier = Modifier.addTag(id = R.string.id_text)
+            )
+        }
+    }
+
+    object Removed: TextState {
+
+        @Composable
+        override fun Apply() = Unit
+    }
+}
+
+private fun Modifier.addTag(@StringRes id: Int) = composed {
+    this.testTag(stringResource(id = id))
 }
