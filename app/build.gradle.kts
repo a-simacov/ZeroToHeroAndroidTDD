46c771058bdaf096f0809c2793d3d5ef2bfe75f4
slax <slax.xals@gmail.com>
2024-01-19 20:37:08 +0200

    task/015 solved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,16 +33,40 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.2"
+    }
 }
 
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation(platform("androidx.compose:compose-bom:2023.04.00"))
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
+
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.4")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4-android:1.5.4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.4")
+
+
 }
--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task015Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task015Test.kt
@@ -4,15 +4,22 @@ import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.hasParent
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.printToLog
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.isEnabled
-import androidx.test.espresso.matcher.ViewMatchers.isNotEnabled
-import androidx.test.espresso.matcher.ViewMatchers.isRoot
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withParent
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -39,37 +46,91 @@ class Task015Test {
         onView(
             allOf(
                 withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.rootLayout)),
+//                withParent(withId(R.id.rootLayout)),
                 isAssignableFrom(ProgressBar::class.java),
-                withId(R.id.progressBar)
+//                withId(R.id.progressBar)
             )
         ).check(matches(not(isDisplayed())))
 
         onView(
             allOf(
                 withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.rootLayout)),
+//                withParent(withId(R.id.rootLayout)),
                 isAssignableFrom(TextView::class.java),
-                withId(R.id.titleTextView)
+//                withId(R.id.titleTextView)
             )
         ).check(matches(not(isDisplayed())))
 
         onView(
             allOf(
                 withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.rootLayout)),
+//                withParent(withId(R.id.rootLayout)),
                 isAssignableFrom(Button::class.java),
-                withId(R.id.actionButton),
+//                withId(R.id.actionButton),
                 withText("load")
             )
         ).perform(click())
 
-        onView(withId(R.id.actionButton)).check(matches(isNotEnabled()))
-        onView(withId(R.id.progressBar)).check(matches(isDisplayed()))
+//        onView(withId(R.id.actionButton)).check(matches(isNotEnabled()))
+//        onView(withId(R.id.progressBar)).check(matches(isDisplayed()))
+//
+//        onView(isRoot()).perform(waitTillDisplayed(R.id.titleTextView, 3500))
+//
+//        onView(withId(R.id.progressBar)).check(matches(not(isDisplayed())))
+//        onView(withId(R.id.actionButton)).check(matches(isEnabled()))
+    }
+}
 
-        onView(isRoot()).perform(waitTillDisplayed(R.id.titleTextView, 3500))
+@RunWith(AndroidJUnit4::class)
+class Task015TestCompose {
 
-        onView(withId(R.id.progressBar)).check(matches(not(isDisplayed())))
-        onView(withId(R.id.actionButton)).check(matches(isEnabled()))
+    @get:Rule
+    val rule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun test_progress() {
+        rule.onRoot(useUnmergedTree = false).printToLog("TERT")
+
+        val columnTag = rule.activity.getString(R.string.id_column)
+        val progressTag = rule.activity.getString(R.string.id_progress)
+        val textTag = rule.activity.getString(R.string.id_text)
+        val btnLoadTag = rule.activity.getString(R.string.id_load_button)
+
+        val btnLoadCaption = rule.activity.getString(R.string.btn_load_caption)
+
+        val btnLoad = rule.onNode(
+            hasTestTag(btnLoadTag)
+                    and
+                    hasText(btnLoadCaption)
+                    and
+                    hasParent(hasTestTag(columnTag))
+        )
+        val text = rule.onNode(
+            hasTestTag(textTag)
+                    and
+                    hasParent(hasTestTag(columnTag))
+        )
+        val progress = rule.onNode(
+            hasTestTag(progressTag)
+                    and
+                    hasParent(hasTestTag(columnTag))
+        )
+
+        progress.assertDoesNotExist()
+        text.assertDoesNotExist()
+        btnLoad.assertIsEnabled()
+
+        btnLoad.performClick()
+
+        btnLoad.assertIsNotEnabled()
+        progress.assertIsDisplayed()
+
+        // NOTE: Важно, таймаут устанавливать не менее, чем в 1.5 раза больше времени ожидания
+        rule.waitUntil(3200) {
+            rule.onAllNodesWithTag(textTag).fetchSemanticsNodes().isNotEmpty()
+        }
+
+        btnLoad.assertIsEnabled()
+        progress.assertDoesNotExist()
     }
 }
