ecb8e941ca7cf3bbab49646889d8462de6ac57be
slax <slax.xals@gmail.com>
2024-01-23 23:41:43 +0200

    task/021 solved

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task021Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task021Test.kt
@@ -3,15 +3,23 @@ package ru.easycode.zerotoheroandroidtdd
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.hasParent
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.test.printToLog
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.isEnabled
 import androidx.test.espresso.matcher.ViewMatchers.isNotEnabled
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withParent
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -34,18 +42,18 @@ class Task021Test {
         onView(
             allOf(
                 withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.rootLayout)),
+//                withParent(withId(R.id.rootLayout)),
                 isAssignableFrom(TextInputEditText::class.java),
-                withId(R.id.inputEditText)
+//                withId(R.id.inputEditText)
             )
         ).check(matches(withText("")))
 
         onView(
             allOf(
                 withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.rootLayout)),
+//                withParent(withId(R.id.rootLayout)),
                 isAssignableFrom(Button::class.java),
-                withId(R.id.actionButton),
+//                withId(R.id.actionButton),
                 withText("change")
             )
         ).check(matches(isNotEnabled()))
@@ -53,33 +61,99 @@ class Task021Test {
         onView(
             allOf(
                 withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.rootLayout)),
+//                withParent(withId(R.id.rootLayout)),
                 isAssignableFrom(TextView::class.java),
-                withId(R.id.titleTextView),
+//                withId(R.id.titleTextView),
                 withText("Hello World!")
             )
         ).check(matches(isDisplayed()))
 
-        onView(withId(R.id.inputEditText)).perform(typeText("m"))
-        onView(withId(R.id.inputEditText)).check(matches(withText("m")))
-        onView(withId(R.id.actionButton)).check(matches(isNotEnabled()))
-
-        onView(withId(R.id.inputEditText)).perform(typeText("i"))
-        onView(withId(R.id.inputEditText)).check(matches(withText("mi")))
-        onView(withId(R.id.actionButton)).check(matches(isNotEnabled()))
-
-        onView(withId(R.id.inputEditText)).perform(typeText("n"))
-        onView(withId(R.id.inputEditText)).check(matches(withText("min")))
-        onView(withId(R.id.actionButton)).check(matches(isEnabled()))
-
-        onView(withId(R.id.actionButton)).perform(click())
-        onView(withId(R.id.inputEditText)).check(matches(withText("")))
-        onView(withId(R.id.titleTextView)).check(matches(withText("min")))
-        onView(withId(R.id.actionButton)).check(matches(isNotEnabled()))
+//        onView(withId(R.id.inputEditText)).perform(typeText("m"))
+//        onView(withId(R.id.inputEditText)).check(matches(withText("m")))
+//        onView(withId(R.id.actionButton)).check(matches(isNotEnabled()))
+//
+//        onView(withId(R.id.inputEditText)).perform(typeText("i"))
+//        onView(withId(R.id.inputEditText)).check(matches(withText("mi")))
+//        onView(withId(R.id.actionButton)).check(matches(isNotEnabled()))
+//
+//        onView(withId(R.id.inputEditText)).perform(typeText("n"))
+//        onView(withId(R.id.inputEditText)).check(matches(withText("min")))
+//        onView(withId(R.id.actionButton)).check(matches(isEnabled()))
+//
+//        onView(withId(R.id.actionButton)).perform(click())
+//        onView(withId(R.id.inputEditText)).check(matches(withText("")))
+//        onView(withId(R.id.titleTextView)).check(matches(withText("min")))
+//        onView(withId(R.id.actionButton)).check(matches(isNotEnabled()))
 
         activityScenarioRule.scenario.recreate()
-        onView(withId(R.id.inputEditText)).check(matches(withText("")))
-        onView(withId(R.id.titleTextView)).check(matches(withText("min")))
-        onView(withId(R.id.actionButton)).check(matches(isNotEnabled()))
+//        onView(withId(R.id.inputEditText)).check(matches(withText("")))
+//        onView(withId(R.id.titleTextView)).check(matches(withText("min")))
+//        onView(withId(R.id.actionButton)).check(matches(isNotEnabled()))
+    }
+}
+
+@RunWith(AndroidJUnit4::class)
+class Task021TestCompose {
+
+    @get:Rule
+    val rule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun test_watch_input() {
+        rule.onRoot(useUnmergedTree = false).printToLog("TERT")
+
+        val rootLayoutTag = rule.activity.getString(R.string.rootLayout)
+        val editTextTag = rule.activity.getString(R.string.inputEditText)
+        val textTag = rule.activity.getString(R.string.titleTextView)
+        val btnActionTag = rule.activity.getString(R.string.actionButton)
+
+        val btnActionCaption = rule.activity.getString(R.string.actionButtonCaption)
+
+        val editText = rule.onNode(
+            hasTestTag(editTextTag)
+                    and
+                    hasParent(hasTestTag(rootLayoutTag))
+        )
+        val btnAction = rule.onNode(
+            hasTestTag(btnActionTag)
+                    and
+                    hasText(btnActionCaption)
+                    and
+                    hasParent(hasTestTag(rootLayoutTag))
+        )
+        val text = rule.onNode(
+            hasTestTag(textTag)
+                    and
+                    hasParent(hasTestTag(rootLayoutTag))
+        )
+
+        editText.assertTextEquals("")
+        btnAction.assertIsNotEnabled()
+        text.assertTextEquals("Hello World!")
+
+        editText.performTextInput("m")
+        editText.assertTextEquals("m")
+        btnAction.assertIsNotEnabled()
+
+        editText.performTextInput("i")
+        editText.assertTextEquals("mi")
+        btnAction.assertIsNotEnabled()
+
+        editText.performTextInput("n")
+        editText.assertTextEquals("min")
+        btnAction.assertIsEnabled()
+
+        btnAction.performClick()
+        editText.assertTextEquals("")
+        text.assertTextEquals("min")
+        btnAction.assertIsNotEnabled()
+
+        ViewActions.closeSoftKeyboard()
+
+        rule.activityRule.scenario.recreate()
+
+        editText.assertTextEquals("")
+        text.assertTextEquals("min")
+        btnAction.assertIsNotEnabled()
     }
 }
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,96 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            MainScreen()
+        }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+
+    var text by rememberSaveable {
+        mutableStateOf("Hello World!")
+    }
+    var editText by rememberSaveable {
+        mutableStateOf("")
+    }
+    val enabled by remember {
+        derivedStateOf {
+            editText.length >= 3
+        }
+    }
+    val onChange: (String) -> Unit = {
+        editText = it
+    }
+    val onClick = {
+        text = editText
+        editText = ""
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .addTag(id = R.string.rootLayout),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = editText,
+            onValueChange = onChange,
+            modifier = Modifier.addTag(id = R.string.inputEditText)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Button(
+            onClick = onClick,
+            enabled = enabled,
+            modifier = Modifier.addTag(id = R.string.actionButton)
+        ) {
+            Text(
+                text = stringResource(id = R.string.actionButtonCaption)
+            )
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = text,
+            modifier = Modifier.addTag(id = R.string.titleTextView)
+        )
+    }
+}
+
+fun Modifier.addTag(@StringRes id: Int) = composed {
+    this.testTag(stringResource(id = id))
 }
