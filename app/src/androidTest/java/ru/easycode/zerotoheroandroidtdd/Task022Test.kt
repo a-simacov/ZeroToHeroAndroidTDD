9a3d61eb1a565645d16173641768a72dd68ce253
slax <slax.xals@gmail.com>
2024-01-24 22:32:05 +0200

    task/022 solved

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task022Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task022Test.kt
@@ -1,22 +1,18 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
-import androidx.test.espresso.assertion.PositionAssertions.isCompletelyBelow
-import androidx.test.espresso.assertion.PositionAssertions.isCompletelyRightOf
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withParent
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.hasParent
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onChildren
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.test.printToLog
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.android.material.textfield.TextInputEditText
-import org.hamcrest.Matchers.allOf
 import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
@@ -30,119 +26,192 @@ class Task022Test {
 
     @Test
     fun test_list() {
-        onView(
-            allOf(
-                withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.topLayout)),
-                isAssignableFrom(TextInputEditText::class.java),
-                withId(R.id.inputEditText)
-            )
-        ).check(matches(withText("")))
+//        onView(
+//            allOf(
+//                withParent(isAssignableFrom(LinearLayout::class.java)),
+//                withParent(withId(R.id.topLayout)),
+//                isAssignableFrom(TextInputEditText::class.java),
+//                withId(R.id.inputEditText)
+//            )
+//        ).check(matches(withText("")))
 
-        onView(
-            allOf(
-                withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.topLayout)),
-                isAssignableFrom(Button::class.java),
-                withId(R.id.actionButton),
-                withText("create")
-            )
-        ).check(isCompletelyRightOf(withId(R.id.inputEditText)))
+//        onView(
+//            allOf(
+//                withParent(isAssignableFrom(LinearLayout::class.java)),
+//                withParent(withId(R.id.topLayout)),
+//                isAssignableFrom(Button::class.java),
+//                withId(R.id.actionButton),
+//                withText("create")
+//            )
+//        ).check(isCompletelyRightOf(withId(R.id.inputEditText)))
 
-        onView(withId(R.id.inputEditText)).perform(typeText("first text"))
-        onView(withId(R.id.actionButton)).perform(click())
-        onView(withId(R.id.inputEditText)).check(matches(withText("")))
+//        onView(withId(R.id.inputEditText)).perform(typeText("first text"))
+//        onView(withId(R.id.actionButton)).perform(click())
+//        onView(withId(R.id.inputEditText)).check(matches(withText("")))
 
-        onView(
-            allOf(
-                withText("first text"),
-                withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.contentLayout)),
-                isAssignableFrom(TextView::class.java),
-            )
-        ).check(isCompletelyBelow(withId(R.id.inputEditText)))
+//        onView(
+//            allOf(
+//                withText("first text"),
+//                withParent(isAssignableFrom(LinearLayout::class.java)),
+//                withParent(withId(R.id.contentLayout)),
+//                isAssignableFrom(TextView::class.java),
+//            )
+//        ).check(isCompletelyBelow(withId(R.id.inputEditText)))
 
-        onView(withId(R.id.inputEditText)).perform(typeText("second text"))
-        onView(withId(R.id.actionButton)).perform(click())
-        onView(withId(R.id.inputEditText)).check(matches(withText("")))
+//        onView(withId(R.id.inputEditText)).perform(typeText("second text"))
+//        onView(withId(R.id.actionButton)).perform(click())
+//        onView(withId(R.id.inputEditText)).check(matches(withText("")))
 
-        onView(
-            allOf(
-                withText("first text"),
-                withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.contentLayout)),
-                isAssignableFrom(TextView::class.java),
-            )
-        ).check(isCompletelyBelow(withId(R.id.inputEditText)))
+//        onView(
+//            allOf(
+//                withText("first text"),
+//                withParent(isAssignableFrom(LinearLayout::class.java)),
+//                withParent(withId(R.id.contentLayout)),
+//                isAssignableFrom(TextView::class.java),
+//            )
+//        ).check(isCompletelyBelow(withId(R.id.inputEditText)))
 
-        onView(
-            allOf(
-                withText("second text"),
-                withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.contentLayout)),
-                isAssignableFrom(TextView::class.java),
-            )
-        ).check(isCompletelyBelow(withText("first text")))
+//        onView(
+//            allOf(
+//                withText("second text"),
+//                withParent(isAssignableFrom(LinearLayout::class.java)),
+//                withParent(withId(R.id.contentLayout)),
+//                isAssignableFrom(TextView::class.java),
+//            )
+//        ).check(isCompletelyBelow(withText("first text")))
 
-        onView(withId(R.id.inputEditText)).perform(typeText("third text"))
-        onView(withId(R.id.actionButton)).perform(click())
-        onView(withId(R.id.inputEditText)).check(matches(withText("")))
+//        onView(withId(R.id.inputEditText)).perform(typeText("third text"))
+//        onView(withId(R.id.actionButton)).perform(click())
+//        onView(withId(R.id.inputEditText)).check(matches(withText("")))
 
-        onView(
-            allOf(
-                withText("first text"),
-                withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.contentLayout)),
-                isAssignableFrom(TextView::class.java),
-            )
-        ).check(isCompletelyBelow(withId(R.id.inputEditText)))
+//        onView(
+//            allOf(
+//                withText("first text"),
+//                withParent(isAssignableFrom(LinearLayout::class.java)),
+//                withParent(withId(R.id.contentLayout)),
+//                isAssignableFrom(TextView::class.java),
+//            )
+//        ).check(isCompletelyBelow(withId(R.id.inputEditText)))
 
-        onView(
-            allOf(
-                withText("second text"),
-                withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.contentLayout)),
-                isAssignableFrom(TextView::class.java),
-            )
-        ).check(isCompletelyBelow(withText("first text")))
+//        onView(
+//            allOf(
+//                withText("second text"),
+//                withParent(isAssignableFrom(LinearLayout::class.java)),
+//                withParent(withId(R.id.contentLayout)),
+//                isAssignableFrom(TextView::class.java),
+//            )
+//        ).check(isCompletelyBelow(withText("first text")))
 
-        onView(
-            allOf(
-                withText("third text"),
-                withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.contentLayout)),
-                isAssignableFrom(TextView::class.java),
-            )
-        ).check(isCompletelyBelow(withText("second text")))
+//        onView(
+//            allOf(
+//                withText("third text"),
+//                withParent(isAssignableFrom(LinearLayout::class.java)),
+//                withParent(withId(R.id.contentLayout)),
+//                isAssignableFrom(TextView::class.java),
+//            )
+//        ).check(isCompletelyBelow(withText("second text")))
 
 
-        activityScenarioRule.scenario.recreate()
+//        activityScenarioRule.scenario.recreate()
 
-        onView(
-            allOf(
-                withText("first text"),
-                withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.contentLayout)),
-                isAssignableFrom(TextView::class.java),
-            )
-        ).check(isCompletelyBelow(withId(R.id.inputEditText)))
+//        onView(
+//            allOf(
+//                withText("first text"),
+//                withParent(isAssignableFrom(LinearLayout::class.java)),
+//                withParent(withId(R.id.contentLayout)),
+//                isAssignableFrom(TextView::class.java),
+//            )
+//        ).check(isCompletelyBelow(withId(R.id.inputEditText)))
 
-        onView(
-            allOf(
-                withText("second text"),
-                withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.contentLayout)),
-                isAssignableFrom(TextView::class.java),
-            )
-        ).check(isCompletelyBelow(withText("first text")))
+//        onView(
+//            allOf(
+//                withText("second text"),
+//                withParent(isAssignableFrom(LinearLayout::class.java)),
+//                withParent(withId(R.id.contentLayout)),
+//                isAssignableFrom(TextView::class.java),
+//            )
+//        ).check(isCompletelyBelow(withText("first text")))
 
-        onView(
-            allOf(
-                withText("third text"),
-                withParent(isAssignableFrom(LinearLayout::class.java)),
-                withParent(withId(R.id.contentLayout)),
-                isAssignableFrom(TextView::class.java),
-            )
-        ).check(isCompletelyBelow(withText("second text")))
+//        onView(
+//            allOf(
+//                withText("third text"),
+//                withParent(isAssignableFrom(LinearLayout::class.java)),
+//                withParent(withId(R.id.contentLayout)),
+//                isAssignableFrom(TextView::class.java),
+//            )
+//        ).check(isCompletelyBelow(withText("second text")))
+    }
+}
+
+@RunWith(AndroidJUnit4::class)
+class Task022TestCompose {
+
+    @get:Rule
+    val rule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun test_list() {
+        rule.onRoot(useUnmergedTree = false).printToLog("TERT")
+
+        val topLayoutTag = rule.activity.getString(R.string.topLayout)
+        val editTextTag = rule.activity.getString(R.string.inputEditText)
+        val btnActionTag = rule.activity.getString(R.string.actionButton)
+        val contentLayoutTag = rule.activity.getString(R.string.contentLayout)
+
+        val btnActionCaption = rule.activity.getString(R.string.actionButtonCaption)
+
+        val editText = rule.onNode(
+            hasTestTag(editTextTag)
+                    and
+                    hasParent(hasTestTag(topLayoutTag))
+        )
+        val btnAction = rule.onNode(
+            hasTestTag(btnActionTag)
+                    and
+                    hasText(btnActionCaption)
+                    and
+                    hasParent(hasTestTag(topLayoutTag))
+        )
+
+        editText.performTextInput("first text")
+        btnAction.performClick()
+        editText.assertTextEquals("")
+
+        val content = rule.onNode(hasTestTag(contentLayoutTag))
+
+        var children = content.onChildren()
+        children.assertCountEquals(1)
+
+        children[0].assertTextEquals("first text")
+
+        editText.performTextInput("second text")
+        btnAction.performClick()
+        editText.assertTextEquals("")
+
+        children = content.onChildren()
+
+        children.assertCountEquals(2)
+        children[0].assertTextEquals("first text")
+        children[1].assertTextEquals("second text")
+
+        editText.performTextInput("third text")
+        btnAction.performClick()
+        editText.assertTextEquals("")
+
+        children = content.onChildren()
+
+        children.assertCountEquals(3)
+        children[0].assertTextEquals("first text")
+        children[1].assertTextEquals("second text")
+        children[2].assertTextEquals("third text")
+
+        rule.activityRule.scenario.recreate()
+
+        children = content.onChildren()
+
+        children.assertCountEquals(3)
+        children[0].assertTextEquals("first text")
+        children[1].assertTextEquals("second text")
+        children[2].assertTextEquals("third text")
     }
 }
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,133 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.toMutableStateList
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            MainScreen()
+        }
     }
+}
+
+@Composable
+fun <T : Any> rememberMutableStateListOf(vararg elements: T): SnapshotStateList<T> {
+    return rememberSaveable(
+        saver = listSaver(
+            save = { stateList ->
+                if (stateList.isNotEmpty())
+                    stateList.toList()
+                else
+                    emptyList<T>()
+            },
+            restore = { it.toMutableStateList() }
+        )
+    ) {
+        elements.toList().toMutableStateList()
+    }
+}
+
+@Composable
+fun MainScreen() {
+
+    val contentList = rememberMutableStateListOf<String>()
+    var editText by rememberSaveable {
+        mutableStateOf("")
+    }
+    val onChange: (String) -> Unit = {
+        editText = it
+    }
+    val onClick = {
+        contentList.add(editText)
+        editText = ""
+    }
+
+    Column {
+        AddListViews(
+            editText = editText,
+            onChange = onChange,
+            onClick = onClick
+        )
+        DataList(contentList)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddListViews(
+    editText: String,
+    onChange: (String) -> Unit,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .addTag(id = R.string.topLayout),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        TextField(
+            value = editText,
+            onValueChange = onChange,
+            modifier = Modifier.addTag(id = R.string.inputEditText)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Button(
+            onClick = onClick,
+            modifier = Modifier.addTag(id = R.string.actionButton)
+        ) {
+            Text(
+                text = stringResource(id = R.string.actionButtonCaption)
+            )
+        }
+    }
+}
+
+@Composable
+fun DataList(contentList: SnapshotStateList<String>) {
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth().addTag(id = R.string.contentLayout),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        items(contentList) {
+            Text(text = it)
+        }
+    }
+}
+
+fun Modifier.addTag(@StringRes id: Int) = composed {
+    this.testTag(stringResource(id = id))
 }
